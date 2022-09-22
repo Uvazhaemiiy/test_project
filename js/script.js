8d4e902f6c24b70660505c7a7d8987e970cc2e5a
Uvazhaemiiy <1191132t@gmail.com>
2022-09-23 00:13:06 +0300

    test2

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//const answer = +prompt("Вам есть 18?", "18")
+//console.log(answer + 5);
+
+
+const user = 'toys';
+
+alert(`Hi, ${user}`);
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
